38689e42b6f497edaea8e233537521750000318f
ixn3rd3mxn <ixn3rd3mxn@gmail.com>
2026-06-17 06:57:00 +0300

    pre-alpha-0.0.18

--- src/app/pages/dashboardclone/dashboard.ts
+++ src/app/pages/dashboardclone/dashboard.ts
@@ -26,7 +26,7 @@ import { RevenueStreamWidget } from './components/revenuestreamwidget';
             </div>
         </div>
         <p-scrolltop />
-        <p-speeddial [model]="items" direction="up" [style]="{ position: 'fixed', right: '1rem', bottom: '1rem' }" />
+        <p-speeddial [model]="items" direction="up" [style]="{ position: 'fixed', right: '1rem', bottom: '1rem' }" [tooltipOptions]="{ tooltipPosition: 'left' }" />
     `,
     styles: [`
         :host ::ng-deep .p-scrolltop {
@@ -43,28 +43,33 @@ export class DashboardCloneComponent implements OnInit {
     ngOnInit() {
         this.items = [
             {
+                label: 'บันทึกข้อมูล',
                 icon: 'pi pi-pencil',
                 command: () => {
                     this.messageService.add({ severity: 'info', summary: 'Add', detail: 'Data Added' });
                 }
             },
             {
+                label: 'สลับวันเวลา',
+                icon: 'pi pi-calendar-clock',
+                command: () => {
+                    this.messageService.add({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
+                }
+            },
+            {
+                label: 'วันเวลาปัจจุบัน',
                 icon: 'pi pi-refresh',
                 command: () => {
                     this.messageService.add({ severity: 'success', summary: 'Update', detail: 'Data Updated' });
                 }
             },
             {
-                icon: 'pi pi-trash',
-                command: () => {
-                    this.messageService.add({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
-                }
-            },
-            {
+                label: 'Upload',
                 icon: 'pi pi-upload',
                 routerLink: ['/fileupload']
             },
             {
+                label: 'Angular.dev',
                 icon: 'pi pi-external-link',
                 target: '_blank',
                 url: 'https://angular.dev'
