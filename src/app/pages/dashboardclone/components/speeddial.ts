4eea0ad141b5c2fb0c427e9c1b30a344e78617c2
ixn3rd3mxn <ixn3rd3mxn@gmail.com>
2026-06-17 12:30:12 +0300

    pre-alpha-0.0.20

--- src/app/pages/dashboardclone/components/speeddial.ts
+++ src/app/pages/dashboardclone/components/speeddial.ts
@@ -1,19 +1,44 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { SpeedDialModule } from 'primeng/speeddial';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { SelectModule } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
 import { MenuItem, MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 
 @Component({
     standalone: true,
     selector: 'app-speed-dial',
-    imports: [ToastModule, SpeedDialModule],
+    imports: [ToastModule, SpeedDialModule, DialogModule, ButtonModule, SelectModule, FormsModule],
     template: `<p-toast />
-    <p-speeddial [model]="items" direction="up" [style]="{ position: 'fixed', right: '1rem', bottom: '1rem' }" [tooltipOptions]="{ tooltipPosition: 'left' }" />`,
+    <p-speeddial [model]="items" direction="up" [style]="{ position: 'fixed', right: '1rem', bottom: '1rem' }" [tooltipOptions]="{ tooltipPosition: 'left' }" />
+    
+    <p-dialog header="บันทึกข้อมูล" [(visible)]="display" [breakpoints]="{ '960px': '75vw' }" [style]="{ width: '30vw' }" [modal]="true">
+        <p-select [(ngModel)]="dropdownValue" [options]="dropdownValues" optionLabel="name" placeholder="Select" class="w-full" appendTo="body" />
+        <ng-template #footer>
+            <p-button label="Save" (click)="close()" />
+        </ng-template>
+    </p-dialog>`,
     providers: [MessageService]
 })
 export class SpeedDial implements OnInit {
     private messageService = inject(MessageService);
     items: MenuItem[] | null = null;
+    display: boolean = false;
+    dropdownValue: any = null;
+
+    dropdownValues = [
+        { name: 'New York', code: 'NY' },
+        { name: 'Rome', code: 'RM' },
+        { name: 'London', code: 'LDN' },
+        { name: 'Istanbul', code: 'IST' },
+        { name: 'Paris', code: 'PRS' }
+    ];
+
+    close() {
+        this.display = false;
+    }
 
     ngOnInit() {
         this.items = [
@@ -21,7 +46,7 @@ export class SpeedDial implements OnInit {
                 label: 'บันทึกข้อมูล',
                 icon: 'pi pi-pencil',
                 command: () => {
-                    this.messageService.add({ severity: 'info', summary: 'Add', detail: 'Data Added' });
+                    this.display = true;
                 }
             },
             {
