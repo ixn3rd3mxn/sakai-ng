00b4ba3d6d92ac6997676c813a17f9e19ae67fb6
ixn3rd3mxn <ixn3rd3mxn@gmail.com>
2026-08-27 13:44:21 +0700

    pre-alpha-0.4.0

--- old/libs/configs.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017")
-db = client["dashboardrescue"]
-
-shiftwork_collection = db["shiftwork"]
-incident_collection = db["incident"]
-cbdcriteria_collection = db["cbdcriteria"]
-cbdlevel_collection = db["cbdlevel"]
